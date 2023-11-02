877375f998d5ecb0276756464b94c923391def15
Michal Ďuračík <Michal.Duracik@fri.uniza.sk>
2023-11-02 19:41:19 +0100

    Fix

--- App/Views/Home/index.view.php
+++ App/Views/Home/index.view.php
@@ -9,7 +9,7 @@
     <div class="row justify-content-center">
 
         <?php foreach ($data["posts"] as $post): ?>
-            <div class="col-3 d-flex gap-4  flex-column">
+            <div class="col-3 d-flex gap-4  flex-column mb-4">
                 <div class="border post d-flex flex-column">
                     <div>
                         <?php
@@ -21,7 +21,7 @@
                         <img src="<?= htmlentities($pictureUrl) ?>" class="img-fluid">
                     </div>
                     <div class="m-2">
-                        <?= htmlentities($post->getText()) ?>
+                        <?= nl2br(htmlentities($post->getText())) ?>
                     </div>
                     <div class="m-2 align-self-end">
                         <a class="btn btn-sm btn-success" href="<?= $link->url("post.edit", ["id" => $post->getId()]) ?>">
