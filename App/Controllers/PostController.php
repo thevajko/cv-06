7348b3970c81cb8f0449e4fc7c80b597415be6c1
hrkut <Patrik.Hrkut@fri.uniza.sk>
2024-10-31 13:24:55 +0100

    Pridavanie prispevkov

--- App/Controllers/PostController.php
+++ App/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Core\AControllerBase;
+use App\Core\Responses\RedirectResponse;
 use App\Core\Responses\Response;
 use App\Models\Post;
 
@@ -18,5 +19,21 @@ class PostController extends AControllerBase
         return $this->html([
             'posts' => $posts
         ]);
+    }
+
+    public function add(): Response
+    {
+        return $this->html();
+    }
+
+    public function save()
+    {
+        $post = new Post();
+        $post->setText($this->request()->getValue('text'));
+        $post->setPicture($this->request()->getValue('picture'));
+
+        $post->save();
+
+        return new RedirectResponse($this->url("post.index"));
     }
 }
